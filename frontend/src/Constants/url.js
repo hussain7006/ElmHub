3dedf9c49a11b39eb0100eed3ef6a66c7e1db0d7
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2025-07-23 18:47:34 +0500

    Update AUTISM, PEOPLE_ANALYTICS, and SAGE URLs to point to the new server IP address

--- frontend/src/Constants/url.js
+++ frontend/src/Constants/url.js
@@ -1,9 +1,9 @@
 export const URL = {
     // AUTISM: 'http://138.197.186.229:5174/autism/',
     NUHA_AI: 'https://nuha.ai:3000/',
-    AUTISM: 'http://localhost:5174/autism/',
+    AUTISM: 'http://141.148.187.5:5174/autism/',
     BALEEG: 'https://nuha.ai:3001/',
     CALLBOT: 'https://baleeg.com:4001/',
-    PEOPLE_ANALYTICS: 'http://localhost:5175/pa/',
-    SAGE: 'http://localhost:5176/sage/',
+    PEOPLE_ANALYTICS: 'http://141.148.187.5:5175/pa/',
+    SAGE: 'http://141.148.187.5:5176/sage/',
 }
