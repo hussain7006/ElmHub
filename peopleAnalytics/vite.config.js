9d39230a9a9c4349e17788eec4f355c1422795a8
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2025-07-30 11:54:04 +0500

    Update vite.config.js to enhance allowedHosts configuration and add HMR settings for improved development experience and secure WebSocket support. PA

--- peopleAnalytics/vite.config.js
+++ peopleAnalytics/vite.config.js
@@ -3,12 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/pa/',
   server: {
     port: 8006,
     host: '0.0.0.0',
-    allowedHosts: ['all'],
-
+    allowedHosts: [
+      'localhost',
+      'people-analytics.baleeg.com',
+      'people-analytics.baleeg.com/pa',
+      'people-analytics.baleeg.com/pa/',
+      'http://150.136.144.219',
+      'http://150.136.144.219:8006',
+      'http://150.136.144.219:8006/pa',
+      'http://150.136.144.219:8006/pa/',
+    ],
+    hmr: {
+      host: 'people-analytics.baleeg.com',
+      protocol: 'wss',
+    },
   },
-  plugins: [react()],
+ 
 })
